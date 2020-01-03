ea8e3607daa285631cd79dabea41e70251f67290
Eun Seok <jeseok95@gmail.com>
2020-01-04 02:16:07 +0900

    정다면체
    cnt 리스트를 만들어서 나오는 수를 하나씩 증가하고 다시 포문을 돌면서 출력하는 방식

--- 섹션1/5.py
+++ 섹션1/5.py
@@ -2,11 +2,17 @@ import sys
 
 sys.stdin = open("./5. 정다면체/in5.txt", "rt")
 
-a, b = map(int, input().split())
+n, m = map(int, input().split())
+cnt = [0] * (n + m)
+max = -2147000000  # 4byte 가장작은수
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        cnt[i + j] += 1
 
-if a > b:
-    for i in range(a - b + 1):
-        print(b + i + 1, end=' ')
-elif a <= b:
-    for i in range(b - a + 1):
-        print(a + i + 1, end=' ')
+for i in range(n + m + 1):
+    if cnt[i] > max:
+        max = cnt[i]
+
+for i in range(n + m + 1):
+    if cnt[i] == max:
+        print(i, end=' ')
