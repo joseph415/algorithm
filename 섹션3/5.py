20335fb2cce99311eb0f73470d139d95aeaedfea
Eun Seok <jeseok95@gmail.com>
2020-01-13 20:17:21 +0900

    회의실 배정
    역으로 정렬해 끝나는 시간을 비교하면 되는 문제
    key값을 설정해주면 되는데 람다함수로 우선순위를 정해준다

---
+++ 섹션3/5.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.stdin = open("./5. 회의실 배정/in6.txt", "rt")
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+a.sort(key=lambda x: (x[1], x[0]))
+print(a)
+cnt = 0
+et = 0
+for s, e in a:
+    print(s, e)
+    if s >= et:
+        et = e
+        cnt += 1
