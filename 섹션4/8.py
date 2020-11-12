4c3ac55e3629959de832246771b0a581708bac7b
Eun Seok <jeseok95@gmail.com>
2020-11-12 18:37:57 +0900

    section4/8

---
+++ 섹션4/8.py
@@ -0,0 +1,19 @@
+import sys
+from collections import deque
+
+sys.stdin = open("./8. 단어찾기/in5.txt", "rt")
+n = int(input())
+p = dict()
+
+for i in range(n):
+    word = input()
+    p[word] = 1
+
+for i in range(n - 1):
+    word = input()
+    p[word] = 0
+
+for i in p.keys():
+    if p[i] == 1:
+        print(i)
+        break
