815610a6ffd1f6bf7319a7337ff7001197180a66
Eun Seok <jeseok95@gmail.com>
2021-03-03 14:33:25 +0900

    4번째문제: h-index
    - 정렬 이용
    - 사고력 기르자

--- programmers/sort/H-index.py
+++ programmers/sort/H-index.py
@@ -10,6 +10,10 @@ def solution(citations):
 
     return answer
 
+# def solution(citations):
+#     citations.sort(reverse=True)
+#     answer = max(map(min, enumerate(citations, start=1)))
+#     return answer
 
 if __name__ == '__main__':
-    print(solution([0, 1, 3, 4, 6]))
+    print(solution([0, 1, 4, 4, 6]))
