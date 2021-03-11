87937beeb16e658fdad217a0062b311f10ca02fd
Eun Seok <jeseok95@gmail.com>
2021-03-11 22:38:09 +0900

    8. 프린터
    - 우선순위 큐만 사용하는게 아니라
    - 덱을 이용해서 계속 뺑뺑이 돌려야한다.

--- programmers/스텍_큐/기능개발.py
+++ programmers/스텍_큐/기능개발.py
@@ -3,19 +3,21 @@ import collections
 
 
 def solution(progresses, speeds):
-    remainTimeRate = collections.deque()
+    remainTime = collections.deque()
     answer = []
 
+    # 배포까지 남은 일 수를 구할 수 있다.
+    # 100 - (현재 진행상황) / (속도 per day) = 남은 일 수 => ceil하는 이유는 2.3 일은 3일이 걸리기 때문
     for i, j in zip(progresses, speeds):
-        remainTimeRate.append(math.ceil((100 - i) / j))
+        remainTime.append(math.ceil((100 - i) / j))
 
-    print(remainTimeRate)
+    print(remainTime)
 
     cnt = 1
-    processingTime = remainTimeRate.popleft()
+    processingTime = remainTime.popleft()
 
-    while remainTimeRate:
-        popleft = remainTimeRate.popleft()
+    while remainTime:
+        popleft = remainTime.popleft()
 
         if popleft > processingTime:
             answer.append(cnt)
@@ -24,7 +26,7 @@ def solution(progresses, speeds):
             continue
         cnt += 1
 
-    # 마지막 처리
+    # [1] 이나 , 마지막 처리를 위해
     answer.append(cnt)
 
     return answer
