b9a0b1d40bde4af50ca8f9530ed3245aa16a4de1
Eun Seok <jeseok95@gmail.com>
2021-04-13 21:05:53 +0900

    순위
    - 플로이드 와샬인데 중간 지점을 k 로 두고 계속 갱신
    - 내가 이긴애들이 이긴애들 1 (나보다 약한애들이 이긴애들이니까)
    - 내가 진애들이 지면 나도 -1 (나보다 쌘 애들이 진거니까)
    https://summa-cum-laude.tistory.com/16
    - 딕셔너리 사용
    - 내가 이긴애들은 내가 진애들을 가져가
    - 내가 진 애들은 내가 이긴애들 가져가
    https://velog.io/@tjdud0123/순위-python
    
    - 컨샙 차이같다

--- programmers/graph/순위.py
+++ programmers/graph/순위.py
@@ -1,3 +1,5 @@
+import collections
+
 import math
 
 matrix = []
@@ -16,33 +18,53 @@ def initMatrix(n, results):
 
 
 # 내가 이긴 놈은 그놈이 이긴 사람 다 이기고, 그 사람은 내가 진놈 다져
+# 플로이드 와샬을 약간 이상하게 for문을 돌림
+# def solution(n, results):
+#     if n == 1:
+#         return 1
+#
+#     answer = 0
+#     initMatrix(n, results)
+#
+#     for i in range(n):
+#         for j in range(n):
+#             if matrix[i][j] == 1:
+#                 for k in range(n):
+#                     if matrix[j][k] == 1 and matrix[i][k] == math.inf:
+#                         matrix[i][k] = 1
+#                         matrix[k][i] = -1
+#                     if matrix[i][k] == -1 and matrix[j][k] == math.inf:
+#                         matrix[j][k] = -1
+#                         matrix[k][j] = 1
+#
+#     for i in matrix:
+#         print(i)
+#
+#     for i in matrix:
+#         if math.inf not in i:
+#             answer += 1
+#
+#     return answer
+# 다른 풀이(해시 딕셔너리)
 def solution(n, results):
-    if n == 1:
-        return 1
-
     answer = 0
-    initMatrix(n, results)
+    wins = collections.defaultdict(set)
+    loses = collections.defaultdict(set)
 
-    for i in range(n):
-        for j in range(n):
-            if matrix[i][j] == 1:
-                for k in range(n):
-                    if matrix[j][k] == 1 and matrix[i][k] == math.inf:
-                        matrix[i][k] = 1
-                        matrix[k][i] = -1
-                    if matrix[i][k] == -1 and matrix[j][k] == math.inf:
-                        matrix[j][k] = -1
-                        matrix[k][j] = 1
+    for a, b in results:
+        wins[a].add(b)
+        loses[b].add(a)
 
-    for i in matrix:
-        print(i)
+    for i in range(1, n + 1):
+        for loser in wins[i]:
+            loses[loser] |= loses[i]
+        for winner in loses[i]:
+            wins[winner] |= wins[i]
 
-    for i in matrix:
-        if math.inf not in i:
+    for i in range(1, n + 1):
+        if len(wins[i]) + len(loses[i]) == n - 1:
             answer += 1
 
     return answer
-
-
 if __name__ == '__main__':
     print(solution(5, [[1, 4], [4, 2], [2, 5], [5, 3]]))
